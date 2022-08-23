24c3db21b84e461a96d78afef1c28703edb46268
Alexandr Dudnyk <kultprosvet87@gmail.com>
2022-08-23 14:43:06 +0300

    Vimeo Done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,17 @@
 import VimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const LOCAL_STOR_KEY = 'videoplayer-current-time';
 
 const iframe = document.querySelector('iframe');
 const player = new VimeoPlayer(iframe);
-console.log(player);
-console.log(iframe);
+
+if (localStorage.getItem(LOCAL_STOR_KEY)) {
+  player.setCurrentTime(localStorage.getItem(LOCAL_STOR_KEY));
+}
+
+player.on('timeupdate', throttle(timeUpdate, 1000));
+
+function timeUpdate({ seconds }) {
+  localStorage.setItem(LOCAL_STOR_KEY, seconds);
+}
