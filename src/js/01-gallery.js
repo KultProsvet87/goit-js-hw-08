272e11be5a688a0fc7ea9a8b442664f0043514f2
Alexandr Dudnyk <kultprosvet87@gmail.com>
2022-08-23 13:58:53 +0300

    Gallery Done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const galleryMarkUp = makeGalleryMarkup(galleryItems);
+galleryEl.innerHTML = galleryMarkUp;
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+gallery.on('show.simplelightbox');
+
+function makeGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>`
+    )
+    .join('');
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,6 @@
+import VimeoPlayer from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new VimeoPlayer(iframe);
+console.log(player);
+console.log(iframe);
